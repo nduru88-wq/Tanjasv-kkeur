95164cf9413501fe704f22528082cb4549371303
nduru88-wq <pmn@specialminds.dk>
2026-04-06 12:31:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,37 @@
-const CACHE_NAME = "alarm-app-v1";
+const CACHE_NAME = "alarm-app-v3";
+
+// Alle filer der skal caches
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/lyd1.mp3",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./lyd1.mp3",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// INSTALL – cacher filer
 self.addEventListener("install", (event) => {
+  console.log("Service Worker: Install");
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+// ACTIVATE – rydder gammel cache
 self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activate");
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
+            console.log("Sletter gammel cache:", key);
             return caches.delete(key);
           }
         })
@@ -32,6 +41,7 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
+// FETCH – henter fra cache først, ellers net
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
