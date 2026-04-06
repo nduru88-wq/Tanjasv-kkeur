e24673fa010de28ee84d7e04edacb1b45572c962
nduru88-wq <pmn@specialminds.dk>
2026-04-06 12:02:17 +0200

    Add service worker installation and caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,14 @@
-
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("alarm-app").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/lyd1.mp3",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
